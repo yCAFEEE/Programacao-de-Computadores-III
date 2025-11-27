a8f8439c0eaa3104078f64a9a470a484c2660a9f
yCAFEEE <yuridan2705@gmail.com>
2025-11-27 18:13:36 -0300

    update props

---
+++ src/components/props/Labelln.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Labelln({ texto }){
+    return(
+        <h1>
+            {texto}
+        </h1>
+    );
+}
--- src/components/props/Login.jsx
+++ src/components/props/Login.jsx
@@ -1,9 +1,17 @@
 import InputText from "./InputText.jsx";
+import Button from "./Button.jsx";
+import Label from "./Label.jsx";
+import Labelln from "./Labelln.jsx";
 
 export default function Login(){
     return(
         <>
-            <InputText placeHolder="usuário" />
+            <Labelln texto="Login" />
+            <Label texto="Usuário" /><br />
+            <InputText placeHolder="usuário" /><br />
+            <Label texto="Senha" /><br />
+            <InputText placeHolder="senha" /><br />
+            <Button valor="Login" />
         </>
     );
 }
